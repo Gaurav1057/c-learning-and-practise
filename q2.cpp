136fa2fcce6c4473e7dc5b7bfd5f18797604d319
Gaurav1057 <gtg20075@gmail.com>
2026-06-12 21:25:37 +0530

    question: sum of n numbers

---
+++ q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int sum=0;
+     int n = 5;
+     for(int i=1; i<=n;i++) {
+      
+     sum =sum+i;
+    
+
+     }
+      
+      
+     cout <<"sum = " << sum << endl;
+
+    return 0;
+}
