bb642188335351ef35dc4f2823630d2e9007e584
Gaurav1057 <gtg20075@gmail.com>
2026-06-12 18:35:41 +0530

    learning day 2

---
+++ loop_for_ex1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+       int n=50;
+       for(int i = 1; i <= n;i++) {
+        
+        cout << i << " ";
+
+        
+
+       }
+
+        cout << endl;
+
+
+
+
+
+    return 0;
+}
