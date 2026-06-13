9610dd10d9ec569beae25498cdf6c358e07e626d
Gaurav1057 <gtg20075@gmail.com>
2026-06-13 15:39:58 +0530

    q4_practise

---
+++ q4_practise.cpp
@@ -0,0 +1,25 @@
+#include <iostream> //finding sum of 1 to n using while loop
+using namespace std;
+
+int main() {
+    int n = 5;
+    int i = 1;
+    int sum = 0;
+    while(i<=n ) { 
+        
+        sum=sum+i;
+          i++;
+
+    }
+      cout << sum ;
+
+    cout << endl ;
+
+
+
+
+
+
+
+    return 0;
+}
