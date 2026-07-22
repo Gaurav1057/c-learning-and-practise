1b5d3a6ccfb5143030d9660c704d273c34da61a2
Gaurav1057 <gtg20075@gmail.com>
2026-07-22 14:03:58 +0530

    functions_ex3.cpp

---
+++ functions_ex3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int sumofDigits(int num) {
+int digitSum = 0;
+while(num > 0) {
+    int lastDig = num%10 ;
+    num /= 10;
+    digitSum += lastDig ;
+}
+return digitSum ;
+}
+int main() {
+    cout << "sum = " << sumofDigits(2356) << endl ;
+    return 0;
+}
