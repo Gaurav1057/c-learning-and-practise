8e35252df747ae3c5110a43bb7ed1cbe08f829fd
Gaurav1057 <gtg20075@gmail.com>
2026-07-22 08:16:13 +0530

    changes

--- nested_loop_ex3.cpp
+++ nested_loop_ex3.cpp
@@ -1,16 +1,22 @@
 #include <iostream> //printing numbers 1 to 9 in vertical order in 3 lines
 using namespace std;
 int main() {
- int num = 1; 
- int n = 3 ;
- for(int i = 0;i<n;i++) {
+ int n = 3;
+int num = 1;
+for(int i = 0;i<n;i++) {
     for(int j = 0;j<n;j++) {
         cout << num ;
         num++;
     }
+    cout << endl ;
+}
 
-cout << endl ;
 
- }
+
+
+
+
+
+
     return 0;
 }
