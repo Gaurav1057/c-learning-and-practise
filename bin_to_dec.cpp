36aae3804f236b7eac0576128378b2b68647ae76
Gaurav1057 <gtg20075@gmail.com>
2026-08-05 14:23:55 +0530

    binary to decimal code

---
+++ bin_to_dec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int BintoDec (int binNum){
+int ans = 0;
+int pow = 1;
+while(binNum > 0) {
+    int rem = binNum % 10 ;
+ans += pow*rem ;
+binNum /= 10 ;
+pow = pow * 2 ;
+}
+return ans ;
+}
+int main() {
+int binNum = 110010 ;
+ cout <<"binary to decimal = " << BintoDec(binNum) << endl ;
+
+    return 0 ;
+}
+
+
+
+
